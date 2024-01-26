dc09d43f7f6319c5b4bbd0e0bf76fc0756c60963
pratyaksh10 <praopratyaksh1@gmail.com>
2024-01-26 16:13:35 -0500

    Training script updated for HPC

--- scripts/sbatch/train.sh
+++ scripts/sbatch/train.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-#                  R   B        model_type     unroll_length  
-sbatch job.sbatch  0   512           tcn             20                 
-sbatch job.sbatch  1   512           tcn             25
-sbatch job.sbatch  2   512           tcn             30
-sbatch job.sbatch  3   512           tcn             35
-sbatch job.sbatch  4   512           gru             20                
-sbatch job.sbatch  5   512           gru             25
-sbatch job.sbatch  6   512           gru             30
-sbatch job.sbatch  7   512           gru             35       
+#                  R   B        model_type     unroll_length      encoder_sizes      decoder_sizes
+sbatch job.sbatch  0   512           mlp             1            '512,256,256'   '2048,1024,1024'
+sbatch job.sbatch  1   512           mlp             10           '512,256,256'   '2048,1024,1024'
+sbatch job.sbatch  2   512           mlp             20           '512,256,256'   '2048,1024,1024'
+sbatch job.sbatch  3   512           lstm             1            '512,256,256'    '1024,512,256'
+sbatch job.sbatch  4   512           lstm            10            '512,256,256'    '1024,512,256'
+sbatch job.sbatch  5   512           lstm            20            '512,256,256'    '1024,512,256'
+sbatch job.sbatch  6   512           gru             1             '512,256,256'    '1024,512,256'
+sbatch job.sbatch  7   512           gru             10             '512,256,256'    '1024,512,256'
+sbatch job.sbatch  8   512           gru             20             '512,256,256'    '1024,512,256'
+sbatch job.sbatch  9   512           tcn             1             '1024,512,512'    '1024,512,256'
+sbatch job.sbatch  10  512           tcn             10            '1024,512,512'    '1024,512,256'
+sbatch job.sbatch  11  512           tcn             20            '1024,512,512'    '1024,512,256'
+sbatch job.sbatch  12  512           transformer      1            '1024,512,512'    '1024,512,256'
+sbatch job.sbatch  13  512           transformer     10            '1024,512,512'    '1024,512,256'
+sbatch job.sbatch  14  512           transformer     20            '1024,512,512'    '1024,512,256'
+             
+   
